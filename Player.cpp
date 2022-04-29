f4b8d320eb3440d1a6c1f81997d84d93a23989d8
Alberto Tomasin <alberto.tomasin@gmail.com>
2022-04-29 19:23:33 +0200

    Update
    addded the board declaration in the player constructor and fixed include errors

--- Player.cpp
+++ Player.cpp
@@ -1,68 +1,131 @@
 #include "player.hpp"
 
-class Player{
-    public:
-        enum piece {x, o, X, O, e};
+enum piece {x, o, X, O, e};
 
-        Player(int player_nr) {
-            if (player_nr != 0 && player_nr != 1)
-                // trows exception
-                throw player_exception{player_exception::index_out_of_bounds,
-                                       "The player number can't be " << player_nr << "!\nIt must be 0 or 1!"};
+Player::Player(int player_nr) {
 
-            this->player_nr = player_nr; // sets the player number
+    //checks if player number is valid otherwise throws an exception
+    if (player_nr != 0 && player_nr != 1)
+        throw player_exception{player_exception::index_out_of_bounds, "The player can only be 0 or 1"};
 
-            memory = new Impl; // initializes the memory
-            memory->next = nullptr; // on the first call, the next pointer is nullptr
-            memory->prev = nullptr; // on the first call, the prev pointer is nullptr
-            // initializes the board
-            memory->board = {
-                    {e, x, e, x, e, x, e, x},
-                    {x, e, x, e, x, e, x, e},
-                    {e, x, e, x, e, x, e, x},
-                    {e, e, e, e, e, e, e, e},
-                    {e, e, e, e, e, e, e, e},
-                    {o, e, o, e, o, e, o, e},
-                    {e, o, e, o, e, o, e, o},
-                    {o, e, o, e, o, e, o, e}
-            };
+    this->player_nr = player_nr; // sets the player number
 
-            std::cout << "Player " << player_nr << " created!\n";
-        }
-            ~Player(){}
-            Player(const Player&){}
-            Player& operator=(const Player&){}
-            piece operator()(int r, int c, int history_offset = 0) const{}
-            void load_board(const string& filename){}
-            void store_board(const string& filename, int history_offset = 0) const{}
-            void init_board(const string& filename) const{}
-            void move(){}
-            bool valid_move() const{}
-            void pop(){}
-            bool wins(int player_nr) const{}
-            bool wins() const{}
-            bool loses(int player_nr) const{}
-            bool loses() const{}
-            int recurrence() const{}
+    memory = new Impl; // initializes the memory
+    memory->next = nullptr; // on the first call, the next pointer is nullptr
+    memory->prev = nullptr; // on the first call, the prev pointer is nullptr
 
-    private:
-        int player_nr;
+    /* creates the starting board */
+    // first row
+    memory->board[0][0] = e;
+    memory->board[0][1] = x;
+    memory->board[0][2] = e;
+    memory->board[0][3] = x;
+    memory->board[0][4] = e;
+    memory->board[0][5] = x;
+    memory->board[0][6] = e;
+    memory->board[0][7] = x;
 
-        struct Impl{
-            pImpl next;
-            pImpl prev;
+    // second row
+    memory->board[1][0] = x;
+    memory->board[1][1] = e;
+    memory->board[1][2] = x;
+    memory->board[1][3] = e;
+    memory->board[1][4] = x;
+    memory->board[1][5] = e;
+    memory->board[1][6] = x;
+    memory->board[1][7] = e;
 
-            piece board[8][8];
-        };
+    //third row
+    memory->board[2][0] = e;
+    memory->board[2][1] = x;
+    memory->board[2][2] = e;
+    memory->board[2][3] = x;
+    memory->board[2][4] = e;
+    memory->board[2][5] = x;
+    memory->board[2][6] = e;
+    memory->board[2][7] = x;
 
-    typedef Impl* pImpl;
+    //fourth row
+    memory->board[3][0] = x;
+    memory->board[3][1] = e;
+    memory->board[3][2] = x;
+    memory->board[3][3] = e;
+    memory->board[3][4] = x;
+    memory->board[3][5] = e;
+    memory->board[3][6] = x;
+    memory->board[3][7] = e;
 
-    pImpl memory; // a pointer to the latest board
-};
-int main(){
+    //fifth row
+    memory->board[4][0] = e;
+    memory->board[4][1] = x;
+    memory->board[4][2] = e;
+    memory->board[4][3] = x;
+    memory->board[4][4] = e;
+    memory->board[4][5] = x;
+    memory->board[4][6] = e;
+    memory->board[4][7] = x;
 
-    Player p1(0);
-    std::cout << "end of the program" << std::endl;
+    //sixth row
+    memory->board[5][0] = x;
+    memory->board[5][1] = e;
+    memory->board[5][2] = x;
+    memory->board[5][3] = e;
+    memory->board[5][4] = x;
+    memory->board[5][5] = e;
+    memory->board[5][6] = x;
+    memory->board[5][7] = e;
 
-    return 0;
+    //seventh row
+    memory->board[6][0] = e;
+    memory->board[6][1] = x;
+    memory->board[6][2] = e;
+    memory->board[6][3] = x;
+    memory->board[6][4] = e;
+    memory->board[6][5] = x;
+    memory->board[6][6] = e;
+    memory->board[6][7] = x;
+
+    //eighth row
+    memory->board[7][0] = x;
+    memory->board[7][1] = e;
+    memory->board[7][2] = x;
+    memory->board[7][3] = e;
+    memory->board[7][4] = x;
+    memory->board[7][5] = e;
+    memory->board[7][6] = x;
+    memory->board[7][7] = e;
+
+    std::cout << "Player " << player_nr << " created!\n";
+} // constructor
+
+/*
+Player::~Player(){
 }
+Player::Player(const Player&){
+}
+Player::piece Player::operator()(int r, int c, int history_offset = 0) const{
+}
+void Player::load_board(const std::string& filename){}
+void Player::store_board(const std::string& filename, int history_offset = 0) const{}
+void Player::init_board(const std::string& filename) const{}
+void Player::move(){}
+bool Player::valid_move() const{}
+void Player::pop(){}
+bool Player::wins(int player_nr) const{}
+bool Player::wins() const{}
+bool Player::loses(int player_nr) const{}
+bool Player::loses() const{}
+int Player::recurrence() const{}
+*/
+int player_nr;
+
+struct Impl{
+    Impl* next;
+    Impl* prev;
+
+    Player::piece board[8][8];
+};
+
+typedef Impl* pImpl;
+
+pImpl memory; // a pointer to the latest board
