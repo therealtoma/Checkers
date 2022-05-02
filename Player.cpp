48701958553e17ae77399d6fb92e5a88d78f7361
Alberto Tomasin <alberto.tomasin@gmail.com>
2022-05-02 15:59:16 +0200

    Update
    updated the board

--- Player.cpp
+++ Player.cpp
@@ -17,80 +17,80 @@ Player::Player(int player_nr) {
     memory->board[0][0] = e;
     memory->board[0][1] = x;
     memory->board[0][2] = e;
-    memory->board[0][3] = x;
+    memory->board[0][3] = e;
     memory->board[0][4] = e;
-    memory->board[0][5] = x;
+    memory->board[0][5] = o;
     memory->board[0][6] = e;
-    memory->board[0][7] = x;
+    memory->board[0][7] = o;
 
     // second row
     memory->board[1][0] = x;
     memory->board[1][1] = e;
     memory->board[1][2] = x;
     memory->board[1][3] = e;
-    memory->board[1][4] = x;
+    memory->board[1][4] = e;
     memory->board[1][5] = e;
-    memory->board[1][6] = x;
+    memory->board[1][6] = o;
     memory->board[1][7] = e;
 
     //third row
     memory->board[2][0] = e;
     memory->board[2][1] = x;
     memory->board[2][2] = e;
-    memory->board[2][3] = x;
+    memory->board[2][3] = e;
     memory->board[2][4] = e;
-    memory->board[2][5] = x;
+    memory->board[2][5] = o;
     memory->board[2][6] = e;
-    memory->board[2][7] = x;
+    memory->board[2][7] = o;
 
     //fourth row
     memory->board[3][0] = x;
     memory->board[3][1] = e;
     memory->board[3][2] = x;
     memory->board[3][3] = e;
-    memory->board[3][4] = x;
+    memory->board[3][4] = e;
     memory->board[3][5] = e;
-    memory->board[3][6] = x;
+    memory->board[3][6] = o;
     memory->board[3][7] = e;
 
     //fifth row
     memory->board[4][0] = e;
     memory->board[4][1] = x;
     memory->board[4][2] = e;
-    memory->board[4][3] = x;
+    memory->board[4][3] = e;
     memory->board[4][4] = e;
-    memory->board[4][5] = x;
+    memory->board[4][5] = o;
     memory->board[4][6] = e;
-    memory->board[4][7] = x;
+    memory->board[4][7] = o;
 
     //sixth row
     memory->board[5][0] = x;
     memory->board[5][1] = e;
     memory->board[5][2] = x;
     memory->board[5][3] = e;
-    memory->board[5][4] = x;
+    memory->board[5][4] = e;
     memory->board[5][5] = e;
-    memory->board[5][6] = x;
+    memory->board[5][6] = o;
     memory->board[5][7] = e;
 
     //seventh row
     memory->board[6][0] = e;
     memory->board[6][1] = x;
     memory->board[6][2] = e;
-    memory->board[6][3] = x;
+    memory->board[6][3] = e;
     memory->board[6][4] = e;
-    memory->board[6][5] = x;
+    memory->board[6][5] = o;
     memory->board[6][6] = e;
-    memory->board[6][7] = x;
+    memory->board[6][7] = o;
 
     //eighth row
     memory->board[7][0] = x;
     memory->board[7][1] = e;
     memory->board[7][2] = x;
     memory->board[7][3] = e;
-    memory->board[7][4] = x;
+    memory->board[7][4] = e;
     memory->board[7][5] = e;
-    memory->board[7][6] = x;
+    memory->board[7][6] = o;
     memory->board[7][7] = e;
 } // constructor
 
@@ -211,6 +211,7 @@ int main(){
     Player p1(1);
     Player p2(p1); // test the copy constructor
     Player::piece pedina = p2(0,0, 100); // test the operator()
+
 
     return 0;
 }
